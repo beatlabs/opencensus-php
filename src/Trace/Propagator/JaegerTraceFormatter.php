0b3dfe4a65ab03a51af6c8eba43e1f01eae40634
Angelos Roussakis <a.roussakis@thebeat.co>
2019-05-29 16:27:24 +0300

    JaegerTraceFormatter completed

--- src/Trace/Propagator/JaegerTraceFormatter.php
+++ src/Trace/Propagator/JaegerTraceFormatter.php
@@ -21,7 +21,7 @@ class JaegerTraceFormatter implements FormatterInterface
      *  NOTE ABOUT {parent-span-id}: Deprecated, most Jaeger clients ignore
      *  on the receiving side, but still include it on the sending side
      */
-    const CONTEXT_HEADER_FORMAT = '/(\w+):(\d+):(\d+):(\d)/';
+    const CONTEXT_HEADER_FORMAT = '/(\w+):(\w+):(\d+):(\d)/';
 
     /**
      * Generate a SpanContext object from the Trace Context header
@@ -36,7 +36,7 @@ class JaegerTraceFormatter implements FormatterInterface
             $traceId = strtolower($matches[1]);
 
             $spanId = array_key_exists(2, $matches) && !empty($matches[2])
-                ? $this->decToHex($matches[2])
+                ? $matches[2]
                 : null;
 
             $isEnabled = array_key_exists(4, $matches) ? $matches[4] == '1' : null;
@@ -67,7 +67,7 @@ class JaegerTraceFormatter implements FormatterInterface
 
         if ($context->spanId()) {
             // spanId
-            $ret .= ':' . $this->hexToDec($context->spanId());
+            $ret .= ':' . $context->spanId();
             // parentSpanId
             $ret .= ':0' ;
         } else {
@@ -81,87 +81,4 @@ class JaegerTraceFormatter implements FormatterInterface
         return $ret;
     }
 
-    /**
-     * Hexadecimal to Decimal conversion
-     *
-     * @param $numstring
-     *
-     * @return float|int|string
-     */
-    private function hexToDec($numstring)
-    {
-        $dec = hexdec($numstring);
-        if ($this->isBigNum($dec)) {
-            return $this->baseConvert($numstring, 16, 10);
-        }
-
-        return $dec;
-    }
-
-    /**
-     * Decimal to Hexadecimal conversion
-     *
-     * @param $numstring
-     *
-     * @return string
-     */
-    private function decToHex($numstring) : string
-    {
-        $int = (int) $numstring;
-        if ($this->isBigNum($int)) {
-            return $this->baseConvert($numstring, 10, 16);
-        }
-
-        return dechex($int);
-    }
-
-    /**
-     * @param $number
-     *
-     * @return bool
-     */
-    private function isBigNum($number) : bool
-    {
-        return $number >= PHP_INT_MAX;
-    }
-
-    /**
-     * Returns a string containing number represented in base toBase.
-     *
-     * @param $numstring
-     * @param $fromBase
-     * @param $toBase
-     *
-     * @return string
-     */
-    private function baseConvert($numstring, $fromBase, $toBase) : string
-    {
-        $chars = "0123456789abcdefghijklmnopqrstuvwxyz";
-        $newstring = substr($chars, 0, $toBase);
-
-        $length = strlen($numstring);
-        $result = '';
-
-        for ($i = 0; $i < $length; $i++) {
-            $number[$i] = strpos($chars, $numstring{$i});
-        }
-
-        do {
-            $divide = 0;
-            $newlen = 0;
-            for ($i = 0; $i < $length; $i++) {
-                $divide = $divide * $fromBase + $number[$i];
-                if ($divide >= $toBase) {
-                    $number[$newlen++] = (int) ($divide / $toBase);
-                    $divide = $divide % $toBase;
-                } elseif ($newlen > 0) {
-                    $number[$newlen++] = 0;
-                }
-            }
-            $length = $newlen;
-            $result = $newstring{$divide} . $result;
-        } while ($newlen != 0);
-
-        return $result;
-    }
 }
